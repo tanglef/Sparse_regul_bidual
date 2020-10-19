c24dfbc1661c134679520a67d770a1a05dd2d7b6
Tanguy Lefort <tanguy_l@outlook.fr>
2020-10-19 23:58:59 +0200

    Prepare the latex report

--- draft/Rando_search.py
+++ draft/Rando_search.py
@@ -83,7 +83,7 @@ def rando_search(x, alpha_1, alpha_2, beta_1, beta_2, gamma, delta, best_store=T
     while(len(omega) != 0):
         p = np.random.choice(omega)
         F_gamma_p = F(gamma[p], a_tilde, b_tilde, omega, x)
-        # TO DO
+        # TO DO: everything
 
 
 
